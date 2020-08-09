b36cc5703e02b73907dbb7fc6e4d34f35aa6d9e4
guyazov <guyazov@gmail.com>
2020-08-09 11:15:22 +0300

    Test only enc-dec masking

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -63,7 +63,7 @@ class MultiheadAttention(nn.Module):
 
         self.k_proj = nn.Linear(self.kdim, embed_dim, bias=bias)
         self.v_proj = nn.Linear(self.vdim, embed_dim, bias=bias)
-        self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
+        self.q_proj  = nn.Linear(embed_dim, embed_dim, bias=bias)
 
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
 
