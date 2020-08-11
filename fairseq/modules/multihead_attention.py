6a23035bf8f28cdcb84e97036ea8d81203a38365
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-11 23:32:33 +0300

    Debug Z

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -356,11 +356,10 @@ class MultiheadAttention(nn.Module):
         attn = torch.bmm(attn_probs, v)  # Thats what I called 'Z' in my summary.
         if self.guy_test:
             attn2 = attn.view(bsz, self.num_heads, tgt_len, self.head_dim).transpose(0, 1)
-            print("Z in layer {} is {}".format(self.guy_test_layer_index, attn2[0, :, : , :]))
+            print("Z in layer {} is {}".format(self.guy_test_layer_index, attn2[0, :, :, :]))
             if self.guy_test_layer_index == 5:
                 exit()
         if self.mask_head is not None:
-
             attn = attn.view(bsz, self.num_heads, tgt_len, self.head_dim).transpose(0, 1)
             attn[self.mask_head, :, :, :] = float(0)
             attn = attn.view(bsz * self.num_heads, tgt_len, self.head_dim)
