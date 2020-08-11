8fe670d92181d99dc7f56aaf7d9816fb3d5abf07
omerlandau <omerlan6@gmail.com>
2020-08-11 23:28:11 +0300

    build generic swap config method

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -355,7 +355,7 @@ class MultiheadAttention(nn.Module):
         )
         """
         assert v is not None
-        attn = torch.bmm(attn_probs, v)  # Thats what I called 'Z' in my summary.
+        attn = torch.bmm(attn_weights, v)  # Thats what I called 'Z' in my summary.
         if self.guy_test:
             attn2 = attn.view(bsz, tgt_len, self.num_heads, self.head_dim).transpose(0, 2)
             print("Z in layer {} is {}".format(self.guy_test_layer_index, attn2[0, :, : , :]))
