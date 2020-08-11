3fc14ab29300433e8adaf38772b14a0242b6f2c8
omerlandau <omerlan6@gmail.com>
2020-08-11 18:50:07 +0300

    build generic swap config method

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -478,7 +478,10 @@ def mhr_single_head(model, head_dim, num_heads, src_parameters, dst_parameters, 
             print(model.state_dict()[s_key].size())
             m = model.state_dict()
             m[s_key] = m[s_key].view(-1, num_heads, head_dim).transpose(0, 1)
-            model.load_state_dict(m)
+            odel.decoder.layers
+            model.5.encoder_attn.k_proj.weight = model.decoder.layers.5.encoder_attn.k_proj.weight.view(-1, num_heads, head_dim).transpose(0, 1)
+            print(model.decoder.layers.5.encoder_attn.k_proj.weight.size())
+            #model.load_state_dict(m)
             # one destination parameter(holds all heads)
             #dst_parameter = model.state_dict()[d_key]
             # Change parameter shape to be able getting specific head
