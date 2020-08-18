fce08c8a5af65a6f46c11550de6df95dd50e3562
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-18 20:02:30 +0300

    Testing confidence

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -361,7 +361,8 @@ class MultiheadAttention(nn.Module):
                     conf_temp += attn_weights.view(self.num_heads, bsz, tgt_len, src_len)[j, batch, :-1, :-1] \
                         .flatten().max()
                 conf.append(conf_temp / bsz)
-                print("conf of head num {0} = {1}".format(j, conf))
+                print("conf of head num {0} = {1}".format(j, conf[j]))
+            exit()
         if confidence_arch == "tgt_word_max_avg":
         # Take max for each source word, than average all
             for j in range(self.num_heads):
@@ -373,7 +374,7 @@ class MultiheadAttention(nn.Module):
                             .max()
                     conf_temp += word_attn_sum / (tgt_len - 1)
                 conf.append(conf_temp / bsz)
-                print("conf of head num {0} = {1}".format(j, conf))
+                print("conf of head num {0} = {1}".format(j, conf[j]))
 
             # worth adding layer number (from trasformer_layer)
             # and  print during training the
