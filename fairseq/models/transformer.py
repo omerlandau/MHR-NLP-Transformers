2afd935f25432d528138c5a89856c7ddbac2b17b
omerlandau <omerlan6@gmail.com>
2020-08-19 21:36:09 +0300

    trying to figure out single head attention for argmax over it

--- fairseq/models/transformer.py
+++ fairseq/models/transformer.py
@@ -289,6 +289,8 @@ class TransformerModel(FairseqEncoderDecoderModel):
             src_lengths=src_lengths,
             return_all_hiddens=return_all_hiddens,
         )
+
+        print(decoder_out)
         return decoder_out, conf
 
     # Since get_normalized_probs is in the Fairseq Model which is not scriptable,
