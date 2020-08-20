906b2a3f05e2f5b92f84cb91b9236b0a69c63afa
omerlandau <omerlan6@gmail.com>
2020-08-20 23:23:20 +0300

    fix conf attribute

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -275,8 +275,8 @@ def train(args, trainer, task, epoch_itr, model, experiment_path):
         if should_stop:
             break
 
-    print(conf)
-    exit()
+    with open(args.mhr_experiment.replace("checkpoints","confs") + "-epoch-{0}".format(epoch_itr.epoch),'wb') as fd:
+        pickle.dump(batches_conf, fd, protocol=pickle.HIGHEST_PROTOCOL)
     # log end-of-epoch stats
     stats = get_training_stats(metrics.get_smoothed_values("train"))
     progress.print(stats, tag="train", step=num_updates)
