d25b634ffbdbe2e04c8fb57438102f4bad0c2f53
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-20 19:56:38 +0300

    Check Pruning2 - add get_dummy_batch to language_pair_dataset

--- fairseq/modules/transformer_layer.py
+++ fairseq/modules/transformer_layer.py
@@ -362,7 +362,7 @@ class TransformerDecoderLayer(nn.Module):
             value=y,
             key_padding_mask=self_attn_padding_mask,
             incremental_state=incremental_state,
-            need_weights=False,
+            need_weights=True,
             attn_mask=self_attn_mask,
         )
         self.self_attn_variables["weights"] = attn
@@ -396,7 +396,7 @@ class TransformerDecoderLayer(nn.Module):
                 key_padding_mask=encoder_padding_mask,
                 incremental_state=incremental_state,
                 static_kv=True,
-                need_weights=need_attn or (not self.training and self.need_attn),
+                need_weights=True,  # (not self.training and self.need_attn)
                 need_head_weights=need_head_weights,
             )
             self.encoder_attn_variables["weights"] = attn
