a4aaf7f4be6cbf4bc91c7c44714486f7029220d0
omerlandau <omerlan6@gmail.com>
2020-08-20 23:05:36 +0300

    fix conf attribute

--- fairseq/sequence_generator.py
+++ fairseq/sequence_generator.py
@@ -262,8 +262,6 @@ class SequenceGenerator(nn.Module):
                 encoder_outs = self.model.reorder_encoder_out(
                     encoder_outs, reorder_state
                 )
-            print("Guy comment -> calling decoder forward from sequence generator")
-                #tokens[:, : step + 1]))
 
             lprobs, avg_attn_scores = self.model.forward_decoder(
                 tokens[:, : step + 1],
