b5416cc735651251e012f410ee51bb657aa1f9db
omerlandau <omerlan6@gmail.com>
2020-08-21 17:09:58 +0300

    fix conf attribute

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -254,9 +254,9 @@ def train(args, trainer, task, epoch_itr, model, experiment_path):
                 continue
 
         for e, d in zip(range(args.encoder_layers), range(args.decoder_layers)):
-            conf["decoder"][d]["self_attn"] = np.append(conf["decoder"][d]["self_attn"], model.decoder.layers[d].self_attn.head_conf, axis=0)
-            conf["decoder"][d]["enc_attn"] = np.append(conf["decoder"][d]["enc_attn"], model.decoder.layers[d].encoder_attn.head_conf,axis=0)
-            conf["encoder"][e]["self_attn"] = np.append(conf["encoder"][e]["self_attn"], model.encoder.layers[e].self_attn.head_conf,axis=0)
+            conf["decoder"][d]["self_attn"] = np.append(conf["decoder"][d]["self_attn"], [model.decoder.layers[d].self_attn.head_conf], axis=0)
+            conf["decoder"][d]["enc_attn"] = np.append(conf["decoder"][d]["enc_attn"], [model.decoder.layers[d].encoder_attn.head_conf],axis=0)
+            conf["encoder"][e]["self_attn"] = np.append(conf["encoder"][e]["self_attn"], [model.encoder.layers[e].self_attn.head_conf],axis=0)
 
         print(conf["encoder"][0]["self_attn"])
 
