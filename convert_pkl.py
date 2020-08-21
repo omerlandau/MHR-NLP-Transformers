3914e0401b4b6ab2fcfa0c5bb510c01fdbea592d
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-21 13:28:50 +0300

    Build khaper code for changing existing pkl files protocol

--- convert_pkl.py
+++ convert_pkl.py
@@ -2,9 +2,11 @@ import pickle
 import os
 directory = "/specific/netapp5_2/gamir/edocohen/guy_and_brian/guy/omer_temp/MHR-runs/confs"
 for filename in os.listdir(directory):
-    with open(os.path.join(directory, filename), 'rb') as fd:
+    file = os.path.join(directory, filename)
+    print(file)
+    with open(file, 'rb') as fd:
         tmp = pickle.load(fd)
     fd.close()
-    with open(os.path.join(directory, filename), 'wb') as fd:
-        pickle.dump(tmp, fd, protocol=3)
-    fd.close()
+    with open(os.path.join(directory, filename), 'wb') as fd2:
+        pickle.dump(tmp, fd2, protocol=3)
+    fd2.close()
