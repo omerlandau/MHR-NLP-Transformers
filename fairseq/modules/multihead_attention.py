0fe4badda2ee1f4d228fbc22fbc1981e314c0a56
omerlandau <omerlan6@gmail.com>
2020-08-21 16:59:58 +0300

    fix conf attribute

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -371,7 +371,7 @@ class MultiheadAttention(nn.Module):
                     heads = heads[0].max(dim=2)
                     heads = heads[0].sum(dim=1)
                     heads = np.array(heads.cpu())
-                    heads = np.append(heads,[bsz])
+                    #heads = np.append(heads,[bsz])
 
                 # Take max for each source word, than average all
                 #for j in range(self.num_heads):
