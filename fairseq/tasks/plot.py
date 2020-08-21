46df2e5f9401a262609f13d66a3ce595390645b7
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-21 10:17:36 +0300

    Prune_test

--- fairseq/tasks/plot.py
+++ fairseq/tasks/plot.py
@@ -1,5 +1,7 @@
+'''
 import pickle
 path = "C:\\Users\\GAZOV\\Desktop\\exp-enc_dec-attn-swaps-layers_04_15-8-heads-6l_with_conf_work_monkey-epoch-36.pkl"
 
 with open(path, 'rb') as f:
     data = pickle.load(f)
+'''
