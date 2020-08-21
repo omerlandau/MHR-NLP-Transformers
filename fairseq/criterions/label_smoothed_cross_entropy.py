ed6e43703d2b991bfd037ea6d0bf68ceb441f5dc
omerlandau <omerlan6@gmail.com>
2020-08-21 19:27:54 +0300

    fix conf attribute

--- fairseq/criterions/label_smoothed_cross_entropy.py
+++ fairseq/criterions/label_smoothed_cross_entropy.py
@@ -57,7 +57,7 @@ class LabelSmoothedCrossEntropyCriterion(FairseqCriterion):
 
         if gamma_conf is not None:
 
-           print(model.encoder.layers[0].self_attn.head_conf.max())
+            print(model.encoder.layers[0].self_attn.head_conf.max())
 
             l_conf = model.encoder.layers[0].self_attn.head_conf.max()[0] - model.encoder.layers[0].self_attn.head_conf.min()[0]
             print(l_conf)
