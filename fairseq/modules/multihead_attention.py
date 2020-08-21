afe44a216edbb6ab5ce6683aac3eaa0a719ffe6a
omerlandau <omerlan6@gmail.com>
2020-08-21 16:55:49 +0300

    fix conf attribute

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -387,7 +387,7 @@ class MultiheadAttention(nn.Module):
 
 
         self.head_conf = conf
-        print(head_conf)
+        print(self.head_conf)
 
         attn_probs = F.dropout(
             attn_weights_float.type_as(attn_weights),
