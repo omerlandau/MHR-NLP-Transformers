9f75b7f18ce9268c09e64f75cb8f9729074ec3ee
omerlandau <omerlan6@gmail.com>
2020-08-22 18:58:45 +0300

    added batch num parameter - starts the conf loss after 40 percent of training is finished. and then decay slowly till the end.

--- fairseq_cli/generate.py
+++ fairseq_cli/generate.py
@@ -273,10 +273,10 @@ def _main(args, output_file):
             conf["decoder"][d]["self_attn"] = np.array(conf["decoder"][d]["self_attn"])
             conf["decoder"][d]["enc_attn"] = np.array(conf["decoder"][d]["enc_attn"])
             conf["encoder"][e]["self_attn"] = np.array(conf["encoder"][e]["self_attn"])
-        path = ''.join(args.path.split('/')[:-1]).replace("checkpoint","confs_eval")
+        path = f''.join(args.path.split('/')[:-1],'/').replace("checkpoint","confs_eval")
         print(path)
 
-        os.mkdir(path, 'rwx')
+        os.mkdir(f"{0}".format(path), 'rwx')
         with open(args.path.replace("checkpoints","confs_eval"), 'wb') as fd:
             pickle.dump(conf, fd, protocol=3)
 
