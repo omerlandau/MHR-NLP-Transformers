b57f44e7d30343e601039e668e37f5f3e47a6214
guyazov <48495031+guyazov@users.noreply.github.com>
2020-08-22 13:01:56 +0300

    Plot testing

--- Guy_tests.py
+++ Guy_tests.py
@@ -7,4 +7,4 @@ def plot_one_head_conf_as_function_of_epoch(experiment,num_of_epochs,head_layer,
       directory = "/specific/netapp5_2/gamir/edocohen/guy_and_brian/guy/omer_temp/MHR-runs/confs/{}".format(experiment)
       for filename in os.listdir(directory):
         print(filename)
-plot_one_head_conf_as_function_of_epoch("exp-enc_dec-attn-swaps-layers_04_15-8-heads-6l_with_conf_work_monkey_gamma_20_enc_g_mm_dec_e_g_start_late_40p_nd_decay",0,0)
+plot_one_head_conf_as_function_of_epoch("exp-enc_dec-attn-swaps-layers_04_15-8-heads-6l_with_conf_work_monkey_gamma_20_enc_g_mm_dec_e_g_start_late_40p_nd_decay",36,0,0)
