2e2a79e24ba28ca72259a7e7de07c91a5611909e
omerlandau <omerlan6@gmail.com>
2020-08-23 01:17:23 +0300

    advanced conf is multiplied by 10 to enlrage changes

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -368,6 +368,7 @@ class MultiheadAttention(nn.Module):
 
             if attn_weights is not None:
                 if(self.head_confidence_method == "base"):
+                    print("hello")
                     a = attn_weights.view(bsz, self.num_heads, tgt_len, src_len).transpose(1,0)
                     heads = a[:, :, :, :].max(dim=3)
                     heads = heads[0].max(dim=2)
