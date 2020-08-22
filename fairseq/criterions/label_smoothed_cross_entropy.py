b7b89a644c85535e21c96771d749c49cd7b569b0
omerlandau <omerlan6@gmail.com>
2020-08-23 00:22:54 +0300

    added batch num parameter - starts the conf loss after 40 percent of training is finished. and then decay slowly till the end.

--- fairseq/criterions/label_smoothed_cross_entropy.py
+++ fairseq/criterions/label_smoothed_cross_entropy.py
@@ -86,7 +86,7 @@ class LabelSmoothedCrossEntropyCriterion(FairseqCriterion):
             l_growth_dec_e = 0
             for i in range(len(model.encoder.layers)):
                 l_conf_enc += model.encoder.layers[i].self_attn.head_conf.var()
-                #l_growth_enc += get_conf_inc_loss_self_driven(model.encoder.layers[i].self_attn.head_conf)
+                l_growth_enc += get_conf_inc_loss_self_driven(model.encoder.layers[i].self_attn.head_conf)
             for i in range(len(model.decoder.layers)):
                 l_conf_dec += model.decoder.layers[i].self_attn.head_conf.var()
                 #l_growth_dec += get_conf_inc_loss_self_driven(model.decoder.layers[i].self_attn.head_conf)
@@ -95,7 +95,9 @@ class LabelSmoothedCrossEntropyCriterion(FairseqCriterion):
 
 
 
-            loss += gamma_conf*(batch_num+0.5)*l_conf_enc + l_conf_dec_e*gamma_conf*(batch_num +0.3) + l_conf_dec*gamma_conf*(batch_num +0.3)
+            loss += gamma_conf*(batch_num+0.4)*l_conf_enc + l_conf_dec_e*gamma_conf*(batch_num +0.3)\
+                    + l_conf_dec*gamma_conf*(batch_num +0.3)\
+                    + l_growth_enc*gamma_conf*(batch_num +0.3)*0.1
 
         logging_output = {
             'loss': loss.data,
