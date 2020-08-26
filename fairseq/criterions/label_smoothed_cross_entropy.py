44c0e93d1926c75a2c81d64a057c83437aa1ee64
omerlandau <omerlan6@gmail.com>
2020-08-27 01:11:48 +0300

    testing - alphas loss

--- fairseq/criterions/label_smoothed_cross_entropy.py
+++ fairseq/criterions/label_smoothed_cross_entropy.py
@@ -4,6 +4,7 @@
 # LICENSE file in the root directory of this source tree.
 
 import math
+import torch
 
 from fairseq import metrics, utils
 from fairseq.criterions import FairseqCriterion, register_criterion
