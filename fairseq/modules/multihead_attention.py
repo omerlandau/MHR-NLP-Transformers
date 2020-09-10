7f6c474bf3b0cfacdbd1bb53139e8207abfd6f59
guyazov <48495031+guyazov@users.noreply.github.com>
2020-09-10 13:34:15 +0300

    cos sim try

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -73,7 +73,7 @@ class MultiheadAttention(nn.Module):
         self.q_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
 
         self.alphas = Parameter(torch.zeros((num_heads, num_heads)))
-        self.alphas_bias = Parameter(torch.zeros(num_heads, 1))
+        #self.alphas_bias = Parameter(torch.zeros(num_heads, 1))
         self.out_proj = nn.Linear(embed_dim, embed_dim, bias=bias)
         self.cosine_similarity_matrix = None
 
@@ -456,8 +456,9 @@ class MultiheadAttention(nn.Module):
         self.cosine_similarity_matrix.requires_grad = False
 
         self.alphas.requires_grad = True
-        self.alphas_bias.requires_grad = True
-        b = torch.mm(self.alphas, b) + self.alphas_bias
+        #self.alphas_bias.requires_grad = True
+        #b = torch.mm(self.alphas, b) + self.alphas_bias
+        b = torch.mm(self.alphas, b)
 
         ctx = b.contiguous().view(self.num_heads, bsz, tgt_len, self.head_dim).transpose(0, 1)
 
