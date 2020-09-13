596d60ce9fc6e71ec0012e750766ecee2db121b0
omerlandau <omerlan6@gmail.com>
2020-09-13 19:13:47 +0300

    fixed cosine similarity

--- fairseq/modules/multihead_attention.py
+++ fairseq/modules/multihead_attention.py
@@ -455,19 +455,11 @@ class MultiheadAttention(nn.Module):
         cos_sim_pairwise = F.cosine_similarity(test_cos, test_cos.unsqueeze(1), dim=-2)
         cos_sim_pairwise = cos_sim_pairwise.permute((2, 0, 1))
 
-        print(cos_sim_pairwise)
-
         cos_sim_pairwise +=1.0
 
         self.cosine_similarity_matrix = cos_sim_pairwise
 
-        print(self.cosine_similarity_matrix)
-
-        cos_sim_pairwise = torch.sum(cos_sim_pairwise, axis=0)/bsz
-
-        print(cos_sim_pairwise)
-
-        exit()
+        #cos_sim_pairwise = torch.sum(cos_sim_pairwise, axis=0)/bsz #used for mean on bsz
 
         cos_sim_pairwise = torch.flatten(cos_sim_pairwise)
 
