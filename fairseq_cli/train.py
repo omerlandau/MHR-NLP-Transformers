10ccc3b8988fc75e5badf524cbdcdea1ca19820f
guyazov <48495031+guyazov@users.noreply.github.com>
2020-09-17 12:01:45 +0300

    Test

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -263,9 +263,10 @@ def train(args, trainer, task, epoch_itr, model, experiment_path, total_samples=
             tgt_tokens = samples[0]['target'][i, :]
             tgt_str = tgt_dict.string(tgt_tokens, escape_unk=True)
             print("Guy comment - > tgt_str is : {}".format(tgt_str))
-            print(model.decoder.layers[0].self_attn_variables["context"][i, 0, :, :])
-            print(model.decoder.layers[0].self_attn_variables["context"][i, 1, :, :])
-            print(model.decoder.layers[0].self_attn_variables["context"][i, 2, :, :])
+            print("Guy comment - > number sent is : {}".format(i))
+            #print(model.decoder.layers[0].self_attn_variables["context"][i, 0, :, :])
+            #print(model.decoder.layers[0].self_attn_variables["context"][i, 1, :, :])
+            #print(model.decoder.layers[0].self_attn_variables["context"][i, 2, :, :])
 
             if log_output is None:  # OOM, overflow, ...
                 continue
