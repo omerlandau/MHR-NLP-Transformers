c21a9f5d5a18e1a43be5eb6f463d2416e86e51db
guyazov <48495031+guyazov@users.noreply.github.com>
2020-09-17 10:33:55 +0300

    Test

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -259,10 +259,10 @@ def train(args, trainer, task, epoch_itr, model, experiment_path, total_samples=
             log_output = trainer.train_step(samples, batch_num=batch_regression)
             tgt_dict = task.target_dictionary
             src_dict = getattr(task, 'source_dictionary', None)
-            print("Guy comment - > samples is : {}".format(samples[0]))
+            #print("Guy comment - > samples is : {}".format(samples[0]))
             #print("Guy comment - > samples['net_input'] is : {}".format(samples[0]['net_input']))
             #print("Guy comment - > samples['net_input']['src_tokens'] is : {}".format(samples[0]['net_input']['src_tokens']))
-            print("Guy comment - > samples['net_input']['target'][i, :] is : {}".format(samples[0]['net_input']['target'][i, :]))
+            print("Guy comment - > samples[0]['target'][i, :] is : {}".format(samples[0]['target'][i, :]))
             tgt_tokens = samples[0]['net_input']['target'][i, :]
             #src_tokens = utils.strip_pad(samples[0]['net_input']['src_tokens'][i, :], tgt_dict.pad())
             #src_str = src_dict.string(src_tokens)
