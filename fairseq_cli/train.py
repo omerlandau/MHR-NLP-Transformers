481f49e7f78e3948749abd67faca5b078c314c68
guyazov <48495031+guyazov@users.noreply.github.com>
2020-09-17 09:41:59 +0300

    Test

--- fairseq_cli/train.py
+++ fairseq_cli/train.py
@@ -257,7 +257,7 @@ def train(args, trainer, task, epoch_itr, model, experiment_path, total_samples=
     for i, samples in enumerate(progress):
         with metrics.aggregate("train_inner"), torch.autograd.profiler.record_function("train_step-%d" % i):
             log_output = trainer.train_step(samples, batch_num=batch_regression)
-
+            print("Guy comment - > {}".format(samples))
             if log_output is None:  # OOM, overflow, ...
                 continue
         total_samples += model.decoder.layers[0].self_attn.bsz
